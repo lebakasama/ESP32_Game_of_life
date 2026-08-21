cf8fb6df5210129fd471e7df25cef347491299b1
Franck Caniard <github@le-franck.com>
2026-08-21 10:00:53 +0900

    Initial commit - first working version

---
+++ src/game_of_life.rs
@@ -0,0 +1,161 @@
+use esp_hal::rng::Rng;
+use heapless::Vec;
+use log::warn;
+
+const NUM_ROWS: usize = 21;
+const NUM_COLS: usize = 40;
+const GRID_LEN: usize = NUM_ROWS * NUM_COLS;
+
+// Row / column to linear index, wrapping
+fn rc_to_i(row: i16, col: i16) -> usize {
+    let num_rows = NUM_ROWS as i16;
+    let num_cols = NUM_COLS as i16;
+    ((row.rem_euclid(num_rows) * num_cols) + col.rem_euclid(num_cols)) as usize
+}
+
+#[derive(Debug)] 
+pub struct GolCoords {
+    pub row: usize,
+    pub col: usize,
+}
+
+impl GolCoords {
+    pub fn from_index(index: usize) -> Self {
+        Self {
+            row: index / NUM_COLS,
+            col: index % NUM_COLS,
+        }
+    }
+}
+
+struct GolGrid {
+    state: [bool; GRID_LEN],
+    state_cache: [bool; GRID_LEN],
+    num_neighbours: [i8; GRID_LEN],
+    num_neighbours_cache: [i8; GRID_LEN],
+}
+
+impl GolGrid {
+    pub fn new_random(rng: &Rng) -> Self {
+        let state: [bool; GRID_LEN] =
+            core::array::from_fn(|_| rng.random().is_multiple_of(3));
+        
+        let mut new_obj = Self {
+            state: state.clone(),
+            state_cache: state,
+            num_neighbours: [0_i8; GRID_LEN],
+            num_neighbours_cache: [0_i8; GRID_LEN],
+        };
+
+        new_obj.init_neighbours();
+
+        new_obj
+    }
+
+    fn init_neighbours(&mut self) {
+        for i in 0..GRID_LEN {
+            if self.state[i] {
+                self.add_to_neighbours(i, 1);
+            }
+        }
+
+        self.num_neighbours = self.num_neighbours_cache;
+    }
+
+    fn add_to_neighbours(&mut self, index: usize, val: i8) {
+        let row = (index / NUM_COLS) as i16;
+        let col = (index % NUM_COLS) as i16;
+        self.num_neighbours_cache[ rc_to_i(row-1, col-1) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row-1, col  ) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row-1, col+1) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row  , col-1) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row  , col+1) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row+1, col-1) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row+1, col  ) ] += val;
+        self.num_neighbours_cache[ rc_to_i(row+1, col+1) ] += val;
+    }
+
+    pub fn kill(&mut self, i: usize) {
+        if self.state[i] == true {
+            self.state_cache[i] = false;
+            self.add_to_neighbours(i, -1);
+        }
+    }
+
+    pub fn spawn(&mut self, i: usize) {
+        if self.state[i] == false {
+            self.state_cache[i] = true;
+            self.add_to_neighbours(i, 1);
+        }
+    }
+
+    pub fn swap_cache(&mut self) {
+        self.state = self.state_cache;
+        self.num_neighbours = self.num_neighbours_cache;
+    }
+}
+
+pub struct GameOfLife {
+    gol_grid: GolGrid,
+    updated: Vec<GolCoords, GRID_LEN>,
+}
+
+impl GameOfLife {
+    pub fn new() -> Self {
+        let mut new_obj = Self {
+            gol_grid: GolGrid::new_random(&Rng::new()),
+            updated: Vec::new(),
+        };
+
+        for i in 0..GRID_LEN {
+            if new_obj.gol_grid.state[i] {
+                new_obj.updated.push(GolCoords::from_index(i)).expect("Push to vec failed");
+            }
+        }
+
+        new_obj
+    }
+
+    pub fn updated(&self) -> &Vec<GolCoords, GRID_LEN> {
+        &self.updated
+    }
+
+    pub fn dimensions(&self) -> (usize, usize) {
+        (NUM_ROWS, NUM_COLS)
+    }
+
+    pub fn alive(&self, row: usize, col: usize) -> bool {
+        self.gol_grid.state[rc_to_i(row as i16, col as i16)]
+    }
+
+    pub fn update(&mut self) {
+        self.updated.clear();
+
+        for i in 0..GRID_LEN {
+            if self.gol_grid.state[i] {
+                // Cell is alive
+                if self.gol_grid.num_neighbours[i] < 2 || self.gol_grid.num_neighbours[i] > 3 {
+                    self.gol_grid.kill(i); 
+                    self.updated.push(GolCoords::from_index(i))
+                        .expect("Failed push to updated cells");
+                }
+            } else {
+                // Cell is dead
+                if self.gol_grid.num_neighbours[i] == 3 {
+                    self.gol_grid.spawn(i);
+                    self.updated.push(GolCoords::from_index(i))
+                        .expect("Failed push to updated cells");
+                }
+            }
+
+            if self.gol_grid.num_neighbours_cache[i] < 0
+                    || self.gol_grid.num_neighbours_cache[i] > 8 {
+                warn!("Bad number of num_neighbours: {}", self.gol_grid.num_neighbours_cache[i]);
+                self.gol_grid.num_neighbours_cache[i] = 0;
+            }
+        }
+
+        self.gol_grid.swap_cache();
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,114 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+mod game_of_life;
+
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{PrimitiveStyle, Rectangle},
+};
+
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output, OutputConfig},
+    spi::master::{Config as SpiConfig, Spi},
+    time::Rate,
+    main,
+};
+use mipidsi::{
+    interface::SpiInterface,
+    models::ST7789,
+    options::ColorOrder,
+    Builder
+};
+//use display_interface_spi::SPIInterface;
+use log::{error};
+
+const DISPLAY_WIDTH: u16 = 172;
+const DISPLAY_HEIGHT: u16 = 320;
+
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    error!("{}", panic_info);
+    loop {}
+}
+
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    // Setup display GPIOS
+    let sclk = peripherals.GPIO1;
+    let mosi = peripherals.GPIO2;
+    let cs = Output::new(peripherals.GPIO14, Level::High, OutputConfig::default());
+    let dc = Output::new(peripherals.GPIO15, Level::Low, OutputConfig::default());
+    let rst = Output::new(peripherals.GPIO22, Level::High, OutputConfig::default());
+    let _bl = Output::new(peripherals.GPIO23, Level::High, OutputConfig::default());
+
+    // Setup SPI (used to transfer commands and data to the display)
+    let spi = Spi::new(peripherals.SPI2,
+                       SpiConfig::default().with_frequency(Rate::from_mhz(40u32)))
+        .unwrap()
+        .with_sck(sclk)
+        .with_mosi(mosi);
+
+    let mut buffer = [0_u8; 512];
+    let spi_dev = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, cs).unwrap();
+    let di = SpiInterface::new(spi_dev, dc, &mut buffer); // SpiDevice, OutputPin
+
+    let mut delay = Delay::new();
+
+    // Display init
+    let mut display = Builder::new(ST7789, di)
+        .display_size(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .display_offset(34, 0)
+        .color_order(ColorOrder::Bgr)
+        .reset_pin(rst)
+        .init(&mut delay)
+        .unwrap();
+    
+    let mut gol = game_of_life::GameOfLife::new();
+    let (num_rows, num_cols) = gol.dimensions();
+
+    delay = Delay::new();
+    let cell_size = (DISPLAY_WIDTH / num_rows as u16).min(DISPLAY_HEIGHT / num_cols as u16);
+
+    // Draw something
+    display.clear(Rgb565::BLACK).unwrap();
+    loop {
+        // Get only the cells that have changed so we don't need to clear and redraw the whole
+        // screen
+        let updated = gol.updated();
+
+        for coord in updated {
+            let colour = if gol.alive(coord.row, coord.col) {Rgb565::RED} else {Rgb565::BLACK};
+
+            Rectangle::new(Point::new(coord.row as i32 * cell_size as i32,
+                                      coord.col as i32 * cell_size as i32),
+                            Size::new(cell_size as u32, cell_size as u32))
+                    .into_styled(PrimitiveStyle::with_fill(colour))
+                    .draw(&mut display)
+                    .unwrap();
+        }
+
+        delay.delay_millis(100);
+        gol.update();
+    }
+}
